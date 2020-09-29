9d4e2adb92543c413f44627ce35772d9a9aaa760
Polina04 <baksheyeva0404@gmail.com>
2020-09-29 13:14:17 +0300

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
+exports.min = function min(array) {
+    if (!Array.isArray(array) || array.length < 1) return 0;
+    const result = Math.min(...array);
+    return result;
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    if (!Array.isArray(array) || array.length < 1) return 0;
+    const result = Math.max(...array);
+    return result;
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    if (!Array.isArray(array) || array.length < 1) return 0;
+    const summ = array.reduce((acc, item) => acc + item);
+    const result = summ / array.length;
+    return result;
+};
